3a563f28a14b1797609831e3d86ed98065017c56
m-kuzmin <71077087+m-kuzmin@users.noreply.github.com>
2024-08-05 16:35:04 +0300

    nix: Added flake.nix
    Added a nix flake file, started tracking Cargo.lock because flakes require that, but exclude it from being published on crates.io

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Minecraft region file (anvil format) reader library in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in rec {
+    formatter.${system} = pkgs.alejandra;
+
+    packages.${system}.default = let
+      manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+    in
+      pkgs.rustPlatform.buildRustPackage {
+        pname = manifest.name;
+        inherit (manifest) version;
+        cargoLock.lockFile = ./Cargo.lock;
+        src = pkgs.lib.cleanSource ./.;
+      };
+
+    devShells.${system}.default = pkgs.mkShell {
+      inputsFrom = [packages.${system}.default];
+
+      buildInputs = with pkgs; [
+        rust-analyzer
+        rustfmt
+        clippy
+      ];
+
+      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    };
+  };
+}
